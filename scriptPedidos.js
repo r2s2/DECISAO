bf27ba55e8d86b6ca449309c0b26237ef0b5ac5f
r2s2 <rodrigo.sacramento@gmail.com>
2024-02-28 23:26:59 -0300

    falta ajustar o acrescentar pedidos

---
+++ scriptPedidos.js
@@ -0,0 +1,30 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var pedido = document.getElementById('caixaPedido').value;
+    var tags = document.getElementById('caixaTagsPedidos').value;
+
+    var data = {
+        pedido: pedido,
+        tagsPedidos: tags
+    };
+
+    fetch('/adiciona_pedidos.html', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(data).toString()
+    }).then(function(response) {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw new Error('Erro ao adicionar pedido');
+        }
+    }).then(function(text) {
+        console.log(text);
+        window.location.href = '/adiciona_pedidos.html';
+    }).catch(function(error) {
+        console.error(error);
+    });
+});
